ed1e171fb98deaba6ed689a06ca78822f6409833
jpitoskas <jpitoskas@gmail.com>
2019-11-19 19:58:36 +0200

    Added incomplete implementation for Dataloading and 2-Layer-NN

---
+++ Lab_1/digitdataset.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+class DigitDataset(Dataset):
+    """ Digits Dataset """
+
+
+    def __init__(self, datatype):
+        assert datatype == 'train' or datatype == 'test'
+        self.datatype = datatype
+        xy = np.loadtxt('./pr_lab1_2016-17_data/' + datatype + '.txt')
+        self.len = xy.shape[0]
+        self.x_data = torch.from_numpy(xy[:, 1:])
+        self.y_data = torch.from_numpy(xy[:, 0])
+
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+
+    def __len__(self):
+        return self.len
---
+++ Lab_1/network_main.py
@@ -0,0 +1,53 @@
+import torch
+import numpy as np
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+from digitdataset import DigitDataset
+from twolayernn import TwoLayerNet
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(  'DEVICE:', DEVICE,
+#         '\ncuda Version:', torch.version.cuda,
+#         '\nn_cuda =', torch.cuda.device_count(),
+#         '\nCuda Name(s):', torch.cuda.get_device_name()    )
+# print(torch.cuda.device(0))
+
+train_set = DigitDataset('train')
+test_set = DigitDataset('test')
+
+train_loader = DataLoader(train_set, batch_size=100, shuffle=True, num_workers=4)
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 100, 256, 100, 1
+
+# Construct our model by instantiating the class defined above
+model = TwoLayerNet(D_in, H, D_out).double()
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+
+epochs = 2
+for epoch in range(epochs):
+    for idx, data in enumerate(train_loader):
+        # get the inputs
+        inputs, labels = data
+
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        # Forward pass: Compute predicted y by passing x to the model
+        y_pred = model(inputs)
+
+        # Compute and print loss
+        print(y_pred.shape, labels.shape)
+        loss = criterion(y_pred, labels)
+        print(epoch, idx, loss.data)
+#
+# #       Zero gradients, perform a backward pass, and update the weights.
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
