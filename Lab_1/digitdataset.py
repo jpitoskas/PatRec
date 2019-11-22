6c7f5d671eff0bf9e780578c88c7d62dc7ea4713
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2019-11-22 04:51:36 +0200

    Modified Neural Net - accuracy is not zero :)

--- Lab_1/digitdataset.py
+++ Lab_1/digitdataset.py
@@ -11,8 +11,8 @@ class DigitDataset(Dataset):
         self.datatype = datatype
         xy = np.loadtxt('./pr_lab1_2016-17_data/' + datatype + '.txt')
         self.len = xy.shape[0]
-        self.x_data = torch.from_numpy(xy[:, 1:]).to(device)
-        self.y_data = torch.from_numpy(xy[:, 0]).to(device)
+        self.x_data = torch.from_numpy(xy[:, 1:]).to(device).float()
+        self.y_data = torch.from_numpy(xy[:, 0]).to(device).long()
 
 
     def __getitem__(self, index):
--- Lab_1/network_main.py
+++ Lab_1/network_main.py
@@ -7,49 +7,47 @@ from twolayernn import TwoLayerNet
 
 # DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DEVICE = "cpu"
-print(  'DEVICE:', DEVICE,
-        '\ncuda Version:', torch.version.cuda,
-        '\nn_cuda =', torch.cuda.device_count(),
-        '\nCuda Name(s):', torch.cuda.get_device_name(0)    )
+if torch.cuda.is_available():
+    print(  'DEVICE:', DEVICE,
+            '\ncuda Version:', torch.version.cuda,
+            '\nn_cuda =', torch.cuda.device_count(),
+            '\nCuda Name(s):', torch.cuda.get_device_name(0)    )
 
 train_set = DigitDataset('train', DEVICE)
 test_set = DigitDataset('test', DEVICE)
 
-train_loader = DataLoader(train_set, batch_size=100, shuffle=True)
-test_loader = DataLoader(test_set, batch_size=100, shuffle=True)
-
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 100, 256, 100, 1
+N, D_in, H, D_out = 100, 256, 100, 10
+
+train_loader = DataLoader(train_set, batch_size=N, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=N, shuffle=True)
 
 # Construct our model by instantiating the class defined above
-model = TwoLayerNet(D_in, H, D_out).double()
+model = TwoLayerNet(D_in, H, D_out)
 # model.to(DEVICE)
 
 # Construct our loss function and an Optimizer. The call to model.parameters()
 # in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
+criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
 #function to calculate accuracy of model
 def accuracy(y_hat, y):
      pred = torch.argmax(y_hat, dim=1)
-     return (pred == y).float().mean()
+     return (pred == y).float().sum()
 
 # TRAINING
 
-print("TRAINING")
+print("TRAINING...")
 
-epochs = 100
+epochs = 1000
 for epoch in range(epochs):
+    model.train()
     for idx, data in enumerate(train_loader):
         # get the inputs
         inputs, labels = data
-
-        # # Use GPU
-        # inputs.to(DEVICE)
-        # labels.to(DEVICE)
 
         # wrap them in Variable
         inputs, labels = Variable(inputs), Variable(labels)
@@ -65,23 +63,25 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print("Epoch:", epoch, "with loss:", loss.item())
+    # print("Epoch:", epoch, "with loss:", loss.item(), "and accuracy:", acc.item()/len(test_set))
 
     # TESTING
 
+    # print("TESTING...")
+    model.eval()
+    acc = 0
     correct = 0
     total = 0
     with torch.no_grad():
         for data in test_loader:
             inputs, labels = data
-            # # Use GPU
-            # inputs.to(DEVICE)
-            # labels.to(DEVICE)
             y_pred = model(inputs)
+            # print(y_pred)
             # _, predicted = torch.max(y_pred.data, 1)
-            # total += labels.size(0)
+            # print(predicted)
+            # print(labels)
             # correct += (predicted.long() == labels.long()).sum().item()
-            acc = accuracy(y_pred.long(), labels.long())
-
-    print('Accuracy of the model: %d %%' % (
-        acc))
+            acc += accuracy(y_pred, labels)
+    if (epoch % 10 == 0):
+        # print('Accuracy of the model:', acc.item()/len(test_set))
+        print("Epoch:", epoch, "with loss:", loss.item(), "and accuracy:", acc.item()/len(test_set))
