cc0ad8e1ad5b39303d16e9981fa90f0aba396cf5
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2019-11-25 02:39:30 +0200

    Added report and code for step 19 (bonusaki)

--- Lab_1/digitdataset.py
+++ Lab_1/digitdataset.py
@@ -1,10 +1,10 @@
 import torch
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
 
 class DigitDataset(Dataset):
     """ Digits Dataset """
-
 
     def __init__(self, datatype, device):
         assert datatype == 'train' or datatype == 'test'
@@ -14,10 +14,24 @@ class DigitDataset(Dataset):
         self.x_data = torch.from_numpy(xy[:, 1:]).to(device).float()
         self.y_data = torch.from_numpy(xy[:, 0]).to(device).long()
 
-
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
-
     def __len__(self):
         return self.len
+
+def DigitDatasetLoader(N, frac, device):
+
+    train_val_set = DigitDataset('train', device)
+    n_train_val = len(train_val_set)
+    n_train = int(frac*n_train_val)
+    n_val = n_train_val - n_train
+    train_set, val_set = data.random_split(train_val_set, (n_train, n_val))
+    test_set = DigitDataset('test', device)
+    n_test = len(test_set)
+
+    train_loader = DataLoader(train_set, batch_size=N, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=N, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=N, shuffle=True)
+
+    return train_loader, n_train, val_loader, n_val, test_loader, n_test
--- Lab_1/network_main.py
+++ Lab_1/network_main.py
@@ -1,40 +1,28 @@
 import torch
 import numpy as np
-from torch.utils import data
+import os
 from torch.autograd import Variable
-from torch.utils.data import Dataset, DataLoader
-from digitdataset import DigitDataset
+import matplotlib.pyplot as plt
+from digitdataset import DigitDataset, DigitDatasetLoader
 from twolayernn import TwoLayerNet
 
-# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DEVICE = "cuda"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# DEVICE = "cpu"
 if torch.cuda.is_available():
     print(  'DEVICE:', DEVICE,
             '\ncuda Version:', torch.version.cuda,
             '\nn_cuda =', torch.cuda.device_count(),
             '\nCuda Name(s):', torch.cuda.get_device_name(0)    )
 
-train_val_set = DigitDataset('train', DEVICE)
-n_train_val = len(train_val_set)
-# 90% Train - 10 % Validation
-n_train = int(0.9*n_train_val)
-n_val = n_train_val - n_train
-train_set, val_set = data.random_split(train_val_set, (n_train, n_val))
-
-test_set = DigitDataset('test', DEVICE)
-n_test = len(test_set)
-
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
 N, D_in, H, D_out = 128, 256, 100, 10
 
-train_loader = DataLoader(train_set, batch_size=N, shuffle=True)
-val_loader = DataLoader(val_set, batch_size=N, shuffle=True)
-test_loader = DataLoader(test_set, batch_size=N, shuffle=True)
+# 90% Train - 10 % Validation
+train_loader, n_train, val_loader, n_val, test_loader, n_test = DigitDatasetLoader(N, 0.9, DEVICE)
 
 # Construct our model by instantiating the class defined above
-model = TwoLayerNet(D_in, H, D_out)
-model.to(DEVICE)
+model = TwoLayerNet(D_in, H, D_out).to(DEVICE)
 
 # Construct our loss function and an Optimizer. The call to model.parameters()
 # in the SGD constructor will contain the learnable parameters of the two
@@ -51,13 +39,24 @@ def train_and_val_model(epochs, criterion, optimizer, model, train, validation, 
 
     # TRAINING
 
+    if os.path.exists("./train.model"):
+        model.load_state_dict(torch.load("./train.model"))
+        # model.eval()
+        print("Loaded from pre-trained model")
+    
     print("TRAINING...")
 
+    total_epochs = []
+    total_acc = []
+    total_loss = []
     for epoch in range(epochs+1):
         model.train()
         for data in train:
             # get the inputs
             inputs, labels = data
+
+            inputs = inputs.to(DEVICE)
+            labels = labels.to(DEVICE)
 
             # wrap them in Variable
             inputs, labels = Variable(inputs), Variable(labels)
@@ -84,8 +83,23 @@ def train_and_val_model(epochs, criterion, optimizer, model, train, validation, 
                 acc += accuracy(y_pred, labels)
         if (epoch % 100 == 0):
             print("Epoch:", epoch, "with loss:", loss.item(), "and accuracy on validation:", acc.item()/n_val)
+        if (epoch % 100 == 0):
+            total_epochs.append(epoch)
+            total_acc.append(acc.item()/n_val)
+            total_loss.append(loss.item())
 
-    torch.save(model.state_dict(), "./train.model")
+    # torch.save(model.state_dict(), "./train.model")
+
+    plt.figure()
+    plt.title("Train and Validation")
+    plt.xlabel("Epochs")
+    plt.plot(total_epochs, total_loss, 'o-', color="r",
+             label="Training Loss")
+    plt.plot(total_epochs, total_acc, 'o-', color="g",
+             label="Validation Accuracy")
+    plt.legend(loc="best")
+    plt.grid()
+    plt.show()
 
 
 def test_model(model, test, n_test):
